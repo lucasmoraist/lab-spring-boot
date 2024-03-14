c0df4fa738c04e3c37acacb3b8bced0867d29a96
lucasmoraist <luksmnt1101@gmail.com>
2024-03-14 09:54:38 -0300

    feat: aprendendo sobre configurações com properties

--- src/main/java/com/example/dio/app/SistemaMensagem.java
+++ src/main/java/com/example/dio/app/SistemaMensagem.java
@@ -1,30 +1,18 @@
 package com.example.dio.app;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @Component
 public class SistemaMensagem implements CommandLineRunner {
-        // Pegando nome a partir do properties
-        // NoReply-DIO -> irá aparecer apenas caso não encontre a váriavel 'name' no properties
-        @Value("${name:NoReply-DIO}")
-        private String nome;
-        @Value("${email}")
-        private String email;
-        @Value("${telefones}")
-        private List<Long> telefones =
-                new ArrayList<>(Arrays.asList(new Long[]{11956781254L}));
-
-        @Override
-        public void run(String... args) throws Exception {
-            System.out.println("Mensagem enviada por: " + nome
-                    + "\nE-mail:" + email
-                    + "\nCom telefones para contato: " + telefones);
-            System.out.println("Seu cadastro foi aprovado");
-        }
+    @Autowired
+    private Remetente rementente;
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Mensagem enviada por: " + rementente.getNome()
+                + "\nE-mail:" + rementente.getEmail() + "\nCom telefones para contato: "
+                + rementente.getTelefones());
+        System.out.println("Seu cadastro foi aprovado");
     }
+}
